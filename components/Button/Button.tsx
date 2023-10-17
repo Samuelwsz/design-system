f7558fef613d6c83ae67a72655e8fd8659af27c7
samuelwsz <samuelxwsz@gmail.com>
2023-10-17 20:19:10 -0300

    att

---
+++ components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export type ButtonProps = {
+  children: React.ReactNode
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
+
+export default function Button({ children, className }: ButtonProps) {
+  return (
+    <>
+      <button
+        className={` bg-[#2D5BFF] rounded-[8px] px-[32px] py-[12px] text-white ${className}`}
+      >
+        {children}
+      </button>
+    </>
+  )
+}
